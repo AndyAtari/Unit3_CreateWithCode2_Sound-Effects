e2974959d95bd69f18dc4e6a92fc41cfbea3af1b
Andy Atari <andrewsalteri@gmail.com>
2021-11-07 13:34:47 -0800

    Destroy object if the obstaceDestroys object if it is an Obstacle and it's past the left boundary

--- MoveLeft.cs
+++ MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 10;
     private PlayerController playerControllerScript;
+    private float leftBound = -13;
 
     // Start is called before the first frame update
     private void Start()
@@ -17,6 +18,12 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOverMan == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        //Destroys object if it is an Obstacle and it's past the left boundary 
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
